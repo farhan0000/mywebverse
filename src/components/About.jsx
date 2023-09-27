4ebfc6cec529518f60c6ac46c6fde66d679cbd09
Farhan Aslam <73159657+farhan0000@users.noreply.github.com>
2023-09-28 01:01:58 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
-import { styles } from "../styles";
+import { styles  } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
